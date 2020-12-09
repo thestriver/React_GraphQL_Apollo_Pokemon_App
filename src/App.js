a5a246ee2fe0e6d6ab9fcf528db46211832ea550
thestriver <wannabeamumin@gmail.com>
2020-12-08 23:47:01 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks"
+import { PokemonContainer } from "./containers/PokemonContainer"
+// import { render } from 'react-dom';
+import "./App.css";
+
+export default function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh'
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonContainer />
+      </main>
+
+    </ApolloProvider>
+  );
+}
---
+++ src/containers/PokemonContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function PokemonContainer() {
+  return (
+    <h1>Hi! I'am a Pokemon Container</h1>
+  )
+}
+
+// {
+//   pokemon(first: 9) {
+//     id
+//     name
+//     attack
+//     created
+//     height
+//     hp
+//     resource_uri
+//     speed
+//     total
+//     weight
+    
+//   }
+// }
