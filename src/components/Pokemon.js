6ddd2a1ae2fe2b9dfb02d2e9b43b9efe356d536e
thestriver <wannabeamumin@gmail.com>
2020-12-09 00:46:02 -0700

    getting queries

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export function Pokemon({ pokemon }) {
-  return <p>{pokemon.name}</p>;
+  return (
+    <div className="pokemons">
+      <div className="pokemon__name">
+        <p>{pokemon.name}</p>
+      </div>
+      <div className="pokemon__meta">
+        <span>{pokemon.maxHP}</span>
+        <span>{pokemon.maxCP}</span>
+      </div>
+      <div className="pokemon__image">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <div className="pokemon__attacks">
+        <p>{pokemon.attacks.special["name"]}</p>
+        <p>{pokemon.attacks.special.damage}</p>
+      </div>
+      <div></div>
+    </div>
+  );
 }
