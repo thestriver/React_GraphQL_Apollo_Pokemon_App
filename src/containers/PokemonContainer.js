801319ff9ce058e00968f855466c74161f297349
thestriver <wannabeamumin@gmail.com>
2020-12-09 20:50:34 -0700

    columnsto3

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -5,7 +5,7 @@ import { GET_POKEMONS } from "../graphql/get-pokemons";
 
 export function PokemonContainer() {
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-    variables: { first: 9 }
+    variables: { first: 12 }
   });
 
   return (
